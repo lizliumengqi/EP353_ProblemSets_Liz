a4796d352a59f65f331bbf6e927753501d01eb01
lizliumengqi <70974925+lizliumengqi@users.noreply.github.com>
2020-09-30 15:50:50 -0400

    corrected mistakes in problem set 3.1

--- ProblemSet3/ProblemSet3.1.c
+++ ProblemSet3/ProblemSet3.1.c
@@ -2,7 +2,10 @@
 #include <math.h>
 
 int main(){
-    char loudness = '-88';
-    //float amp = 
-    //I don't have the book so I'm not sure what the equation is
+    char loudness = -30;
+    float amp;
+    amp = pow (10, loudness/10);
+
+    printf("The loudness of %d is %f", loudness, amp);
+    //I don't have the book so I'm not sure if that's the right equation)
 }
